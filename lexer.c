d9f5c408359e4ed4b3b80d0387938ca1051ace4d
andreius05 <andrejnesterenko055@gmail.com>
2025-03-22 01:28:59 +0100

    Im already can find strings and worked a lot for catch errors

--- lexer.c
+++ lexer.c
@@ -4,6 +4,15 @@
 #include <string.h>
 
 
+typedef enum{
+     SyntaxError,LogicalError,SemanticError,SemiColonError
+}ErrorTypes;
+
+typedef struct error{
+     ErrorTypes typeOfError;
+     int line;
+}error_t;
+
 
 typedef enum commands{
       EXIT,
@@ -18,7 +27,7 @@ typedef enum commands{
 
 
 typedef enum types{
-      INT ,STRING,BOOL,FLOAT
+      INT ,STRING,BOOL,FLOAT,SEPARATOR
 }TokenType;
 
 typedef struct {
@@ -38,50 +47,120 @@ typedef enum syntax_sugar{SEMICOLON=';',OPENBRACKET='(',CLOSEBRACKET=')'};
 
 
 
+void error_call(error_t * error){
+     if (error==NULL)return;
+     if (error->typeOfError==SyntaxError){
+          printf("PROBLEM was in [%d] line this problem conected with SyntaxError pls check youtself)\n",error->line);
+          free(error);
+          return;
+     }
+     if (error->typeOfError==SemiColonError){
+          printf("PROBLEM was in [%d] line this problem conected with SEMICOLONS pls check youtself)\n",error->line);
+          free(error);
+          return;
+     }
+     //Here will some other cases
+}
+
+
+#if 1
 TokenKeyword_t * generate_keyword(char current,FILE * file){
      TokenKeyword_t * token = malloc(sizeof(TokenKeyword_t));
      char * keyword = malloc(64);//MAX KEYWORD LEN NAMEW;
      int index = 0;
      while (isalpha(current)&&current!=EOF){
-	   if (!isalpha(current)){
+          if (index>=63){
+               printf("TOO BIG KEYWORD\n");
+               free(token);
+               free(keyword);
+               return NULL;
+          }
+	     if (!isalpha(current)){
 	       break;
-	   }
+	     }
            keyword[index] = current;
-	   index+=1;
-	   current = fgetc(file);
+	     index+=1;
+	     current = fgetc(file);
      }
-     printf("THIS IS COMMAND [%s] \n",keyword);
+     keyword[index] = '\0';
+
+     
      //HERE are CASES
      if ((strcmp(keyword,"exit")==0)){
          token->command = EXIT;
 	 token->value = keyword;
 	 return token;
      }
+     free(token);
+     free(keyword);
      return NULL;
+}
 
+
+TokenLiteral_t * generate_string(char current,FILE * file){
+     TokenLiteral_t * str_token = malloc(sizeof(TokenLiteral_t));
+     int index = 0;
+     char * buffer = malloc(256);
+     current = fgetc(file);
+     while(current!='"' && current!=EOF){
+          if (index>254){
+               printf("STR is too big man\n");
+               free(str_token);
+               free(buffer);
+               return NULL;
+          }
+          buffer[index] = current;
+          index+=1;
+          current = fgetc(file);
+     }
+     if (current!='"'){
+          free(str_token);
+          free(buffer);
+          return NULL;
+     }
+     buffer[index]='\0';
+     str_token->type = STRING;
+     printf("FOUND STR [%s]",buffer);
+     str_token->value = (char *)buffer;
+     return str_token;
 }
 
 
 TokenLiteral_t * generate_number(char current,FILE * file){
      TokenLiteral_t * token = malloc(sizeof(TokenLiteral_t));
-     token->type = INT;
-     int  * value = malloc(sizeof(int));
-     *value = 0;
-     while (current!=EOF && isdigit(current)){
-           if (!isdigit(current))break;
-	   *value = *value * 10+(current-'0'); 
-	   current = fgetc(file);
+     int is_float = 0;
+     int index = 0;
+     char buffer [64] = {0};
+     
+     while (current!=EOF && (isdigit(current)||current=='.')){
+          if (current=='.'){
+               if (is_float){
+                    break;
+               }
+               is_float = 1;
+          }
+           buffer[index++] = current;
+	      current = fgetc(file);
      }
-
-
      if (current != EOF) {
-        ungetc(current, file);
-    }
-
-     //printf("AYBE THERE\n");
-     printf("value is [%d]",*value);
-     token->value = value;
-     //printf("TEST\n");
+          ungetc(current, file);
+      }
+     if (!is_float){
+          token->type = INT;
+          int * value = malloc(sizeof(int));
+          *value = atoi(buffer);
+          token->value = value ;
+          printf("This number (%d) is INT\n" , *value);
+          
+     }
+     else{
+          token->type = FLOAT;
+          float * value = malloc(sizeof(float));
+          * value = atof(buffer);
+          token->value = value;
+          printf("This number (%f) is FLOAT\n" , *value);
+          
+     }
      return token;
 }
 
@@ -89,10 +168,12 @@ TokenLiteral_t * generate_number(char current,FILE * file){
 void lexer(FILE * file){
      if (file==NULL)return;
      char current = fgetc(file);
+     int index = 0;
      int semicolon = 0;
+     int count_of_correct_str = 0;
      while(current!=EOF){
+          semicolon = 0;
 	  while(current!='\n'){
-          
 	  if (current==';'){
 	      printf("Found SEMICOLON\n");
 	      semicolon+=1;
@@ -106,37 +187,85 @@ void lexer(FILE * file){
 	  }
 	  else if (isdigit(current)){
 	       TokenLiteral_t * test_token = generate_number(current,file);
-	       printf("TOKEN TYPE IS [INT]\n");
-	       printf("AND VALUE IS %d",*(int * )test_token->value);
+            if (test_token->type==INT){
+               printf("TOKEN TYPE IS [INT]\n");
+               printf("AND VALUE IS %d",*(int * )test_token->value);
+            }
+            if (test_token->type==FLOAT){
+               printf("TOKEN TYPE IS [FLOAT]\n");
+               printf("AND VALUE IS %f\n",*(float * )test_token->value);
+            }
 	       printf("\n");
 	       free(test_token->value);
 	       free(test_token);
 	       
 	  }
+       else if (current =='"'){
+          count_of_correct_str+=1;
+          TokenLiteral_t * str_token = generate_string(current,file);
+          if (str_token!=NULL){
+               printf("TYPE of ELEMENT[STR]\n");
+               printf("AND VALUE [%s]",(char*)str_token->value);
+               free(str_token->value);
+               free(str_token);
+          }
+          else{
+               error_t * error = malloc(sizeof(error_t));
+               error->typeOfError = SyntaxError;
+               error->line = index+1;
+               error_call(error);
+               return;
+          }
+       }
 	  else if (isalpha(current)){
 	       TokenKeyword_t * test_tok = generate_keyword(current,file);
-	       printf("HERE IS COMMAND THAT I GOT [%s]\n",(char *)test_tok->value);
+            if (test_tok!=NULL){
+               printf("HERE IS COMMAND THAT I GOT [%s]\n",(char *)test_tok->value);
+               free(test_tok->value);
+               free(test_tok);
+            }
+            else{
+               printf("Undefinded command \n");
+            }
+	       
 	  }
-	  else{printf("%c",current);}
+	  //else{printf("%c",current);}
 	  current = fgetc(file);
 	  }
-	  if (semicolon==0){
-	      printf("Каждую линю закрывай ;\n");
-	      return;
-	  }
-	  if (semicolon>1){
-	      printf("Много слишком таких знаков [;]\n");
-	      return;
-	  }
-	  current = fgetc(file);
+       if (semicolon==0){
+          error_t * error = malloc(sizeof(error_t));
+          error->line = index	+1;
+          error->typeOfError = SemiColonError;
+          error_call(error);      
+      }
+      if (semicolon>1){
+          error_t * error = malloc(sizeof(error_t));
+          error->line = index	+1;
+          error->typeOfError = SemiColonError;
+          error_call(error); 
+      }
+       index+=1;
+       printf("%d line\n",index);
+	  
+	  
+       if (current=='\n'){
+          current = fgetc(file);
+       }
      }
      printf("\n");
 }
-
+#endif
 
 int main(){
     FILE * file;
     file = fopen("test.unn","r");
+    char current = '"';
+    if (isalpha(current)){
+        printf("%c is alpha\n",current);
+    }
+    else{
+        printf("%c is not current\n",current);
+    }
     lexer(file);
     fclose(file);
 }
